091833f5af45ead9ad446dde7fc18e27d63c753f
Leon Bogaert <leon@tim-online.nl>
2018-02-28 23:55:59 +0100

    Convert measurements to valid json

---
+++ measurements_to_csv.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function getlines()
+{
+    echo "{"
+    for file in *.log; do
+        echo "\"$file\": ["
+        cat $file \
+            | grep "{" \
+            | grep "}" \
+        | tr '\n' ','
+        echo "],"
+    done
+    echo "},"
+}
+
+lines=$(getlines)
+# echo $lines; exit
+
+# format json
+echo $lines \
+    | sed "s/N: //g" \
+    | sed 's/trappist/"trappist"/g' \
+    | sed 's/: F/: "F"/g' \
+    | sed 's/: T/: "T"/g' \
+    | sed 's/,\],/\],/g' \
+    | sed 's/\], }/]}/g' \
+    | sed 's/,$//g'
