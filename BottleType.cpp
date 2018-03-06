646b3b68588a9ebc89b6fde77fda892b0e36de41
Leon Bogaert <leon@tim-online.nl>
2018-03-06 10:59:00 +0100

    Remove warnings

--- BottleType.cpp
+++ BottleType.cpp
@@ -22,37 +22,37 @@ void loadBottles(BottleType bottleTypes[MAX_BOTTLE_TYPES])
 {
     Log.notice(F("loadBottles"));
     bottleTypes[0].Name = F("trappist");
-    bottleTypes[0].MinWeight = 102000;
-    bottleTypes[0].MaxWeight = 107500;
-    bottleTypes[0].LiquidWeight = 107000/5;
+    bottleTypes[0].MinWeight = unsigned(102000);
+    bottleTypes[0].MaxWeight = unsigned(107500);
+    bottleTypes[0].LiquidWeight = unsigned(round(107000/5));
 
     bottleTypes[1].Name = F("steinie");
-    bottleTypes[1].MinWeight = 93000;
-    bottleTypes[1].MaxWeight = 96000;
-    bottleTypes[1].LiquidWeight = 107000;
+    bottleTypes[1].MinWeight = unsigned(93000);
+    bottleTypes[1].MaxWeight = unsigned(96000);
+    bottleTypes[1].LiquidWeight = unsigned(107000);
 
 
     bottleTypes[2].Name = F("longneck");
-    bottleTypes[2].MinWeight = 67000;
-    bottleTypes[2].MaxWeight = 70000;
-    bottleTypes[2].LiquidWeight = 107000;
+    bottleTypes[2].MinWeight = unsigned(67000);
+    bottleTypes[2].MaxWeight = unsigned(70000);
+    bottleTypes[2].LiquidWeight = unsigned(107000);
 
 
     bottleTypes[3].Name = F("vichy 33 cl");
-    bottleTypes[3].MinWeight = 77000;
-    bottleTypes[3].MaxWeight = 80000;
-    bottleTypes[3].LiquidWeight = 107000;
+    bottleTypes[3].MinWeight = unsigned(77000);
+    bottleTypes[3].MaxWeight = unsigned(80000);
+    bottleTypes[3].LiquidWeight = unsigned(107000);
 
 
     bottleTypes[4].Name = F("bnr");
-    bottleTypes[4].MinWeight = 81000;
-    bottleTypes[4].MaxWeight = 84000;
-    bottleTypes[4].LiquidWeight = 107000;
+    bottleTypes[4].MinWeight = unsigned(81000);
+    bottleTypes[4].MaxWeight = unsigned(84000);
+    bottleTypes[4].LiquidWeight = unsigned(107000);
 
     bottleTypes[5].Name = F("steinie met beugel");
-    bottleTypes[5].MinWeight = 108000;
-    bottleTypes[5].MaxWeight = 112000;
-    bottleTypes[5].LiquidWeight = 107000;
+    bottleTypes[5].MinWeight = unsigned(108000);
+    bottleTypes[5].MaxWeight = unsigned(112000);
+    bottleTypes[5].LiquidWeight = unsigned(107000);
 }
 
 BottleType::BottleType()
